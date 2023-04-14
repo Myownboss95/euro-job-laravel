51c4951a0d4def4e6b175db9ffd77ad553168427
Xpressglobe <tobeodiachi@yahoo.com>
2023-04-14 03:39:08 +0100

    testing

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $users = User::all();
-        return view('back.admin.dashboard',compact('users'));
+        $deposits = Transaction::where('type', 'deposit')->latest()->get();
+        $transfers = Transaction::where('type', 'transfer')->latest()->get();
+        return view('back.admin.dashboard',compact('users','deposits','transfers'));
     }
 }
--- resources/views/back/admin/dashboard.blade.php
+++ resources/views/back/admin/dashboard.blade.php
@@ -4,9 +4,167 @@
 @section('content')
     <!-- row -->
     <div class="container-fluid">
-        <div class="mb-sm-4 d-flex flex-wrap align-items-center text-head">
-            <h2 class="mb-3 me-auto">Dashboard</h2>
+        <!-- row -->
+<div class="row">
+    <div class="col-xl-3 col-sm-6">
+        <div class="card">
+            <div class="card-body d-flex align-items-center justify-content-between">
+                <div class="card-data me-2">
+                    <h5>Users</h5>
+                    <h2 class="fs-40 font-w600">{{ $users->count() }}</h2>
+                </div>
+                <div><span class="donut1" data-peity='{ "fill": ["var(--primary)", "rgba(242, 246, 252)"]}'>1</span>
+                </div>
+            </div>
         </div>
+    </div>
+    <div class="col-xl-3 col-sm-6">
+        <div class="card">
+            <div class="card-body d-flex align-items-center justify-content-between">
+                <div class="card-data me-2">
+                    <h5>Transfers</h5>
+                    <h2 class="fs-40 font-w600">{{ $transfers->count() }}</h2>
+                </div>
+                <div><span class="donut1" data-peity='{ "fill": ["rgb(56, 226, 93,1)", "rgba(242, 246, 252)"]}'>1</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-sm-6">
+        <div class="card">
+            <div class="card-body d-flex align-items-center justify-content-between">
+                <div class="card-data me-2">
+                    <h5>Deposits</h5>
+                    <h2 class="fs-40 font-w600">{{ $deposits->count() }}</h2>
+                </div>
+                <div><span class="donut1" data-peity='{ "fill": ["rgb(255, 135, 35,1)", "rgba(242, 246, 252)"]}'>1</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
+<div class="row">
+    <div class="col-xl-9 col-xxl-8">
+        <div class="row">
+            {{-- Recent Deposits --}}
+            <div class="col-xl-12">
+                <div class="card">
+                    <div class="card-header border-0  flex-wrap">
+                        <h4 class="fs-20">Deposits</h4>
+                    </div>
+                    <div class="card-body py-0">
+                        <div class="table-responsive">
+                            <table class="table stripped">
+                                <thead>
+                                    <tr>
+                                        <th>Reference</th>
+                                        <th>Amount</th>
+                                        <th>User</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($deposits->take(5) as $deposit )
+
+                                    <tr>
+                                        <td>{{$deposit->reference}}</td>
+                                        <td>{{format_money($deposit->amount,$deposit?->user?->currency?->symbol)}}</td>
+                                        <td>{{$deposit->user->name}}</td>
+                                        <td>{{$deposit->created_at->toDateTimeString()}}</td>
+                                    </tr>
+                                    @empty
+                                    <tr>
+                                        <td colspan="4" class="text-center">No recent deposits!</td>
+                                    </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Recent transfers --}}
+            <div class="col-xl-12">
+                <div class="card">
+                    <div class="card-header border-0  flex-wrap">
+                        <h4 class="fs-20">Debit Transactions</h4>
+                    </div>
+                    <div class="card-body py-0">
+                        <div class="table-responsive">
+                            <table class="table stripped">
+                                <thead>
+                                    <tr>
+                                        <th>Reference</th>
+                                        <th>Amount</th>
+                                        <th>User</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($transfers->take(5) as $withdrawal )
+
+                                    <tr>
+                                        <td>{{$withdrawal->reference}}</td>
+                                        <td>{{format_money($withdrawal->amount,$withdrawal?->user?->currency?->symbol)}}</td>
+                                        <td>{{$withdrawal->user->name}}</td>
+                                        <td>{{$withdrawal->created_at->toDateTimeString()}}</td>
+                                    </tr>
+                                    @empty
+                                    <tr>
+                                        <td colspan="4" class="text-center">No recent transfers!</td>
+                                    </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
+            {{-- Recent Users --}}
+            <div class="col-xl-12">
+                <div class="card">
+                    <div class="card-header border-0  flex-wrap">
+                        <h4 class="fs-20">Recent Users</h4>
+                    </div>
+                    <div class="card-body py-0">
+                        <div class="table-responsive">
+                            <table class="table stripped">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Status</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($users->take(5) as $user )
+
+                                    <tr>
+                                        <td>{{$user->name}}</td>
+                                        <td>{{$user->email}}</td>
+                                        <td>{{$user->status}}</td>
+                                        <td>{{$user->created_at->toDateTimeString()}}</td>
+                                    </tr>
+                                    @empty
+                                    <tr>
+                                        <td colspan="4" class="text-center">No recent users!</td>
+                                    </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
 
 
     </div>
