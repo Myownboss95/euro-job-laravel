d0b9110b1c6c91229851caf12fe9c781e104a975
Xpressglobe <tobeodiachi@yahoo.com>
2023-04-16 05:28:17 +0100

    tests

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -24,7 +24,7 @@ class AdminFactory extends Factory
         return [
             'name' => $this->faker->name('male'),
             'email' => 'admin@'.front_domain(),
-            'password' => '60606060Password@#@#',
+            'password' => '60606060Password@#@#@#',
         ];
     }
 }
