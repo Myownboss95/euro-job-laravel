3258d6c9ecf1a72ce77e040061882f34e9029a46
Xpressglobe <tobeodiachi@yahoo.com>
2023-04-19 19:13:48 +0100

    test

--- app/Http/Controllers/ActivationController.php
+++ app/Http/Controllers/ActivationController.php
@@ -33,7 +33,7 @@ class ActivationController extends Controller
 
         $valid['image'] = str_replace(' ', '', rand() . now()->toDateTimeString() . '.' . $valid['image']->extension());
         $request->file('image')->move(public_path(config('dir.profile')), $valid['image']);
-        dd($valid);
+        // dd($valid);
         $valid['dob'] = Carbon::createFromFormat('Y-m-d', $valid['dob']);
         $valid['account_number'] = generateAccountNumber();
         
