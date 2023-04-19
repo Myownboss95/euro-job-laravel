7e4b9bec3ebe12e76c8884a6be5c060942bbb32a
Xpressglobe <tobeodiachi@yahoo.com>
2023-04-19 18:52:40 +0100

    test

--- app/Http/Controllers/ActivationController.php
+++ app/Http/Controllers/ActivationController.php
@@ -18,28 +18,28 @@ class ActivationController extends Controller
     public function activate(Request $request)
     {
         $valid = $request->validate([
-            'middlename' => 'sometimes|nullable|string',
+            'middlename' => 'nullable|string',
             'dob' => 'required|string',
             'account_type' => 'required|in:' . implode(',', config('constants.account_types')),
-            'phone' => 'required|regex:/^[+][0-9]{9,14}/',
+            //|regex:/^[+][0-9]{9,14}/
+            'phone' => 'required',
             'country' => 'required|string',
             'state' => 'required|string',
             'city' => 'required|string',
             'address' => 'required|string',
             'zip_code' => 'required|string',
-            'image' => 'nullable|mimes:jpeg,jpg,png',
+            'image' => 'required|mimes:jpeg,jpg,png',
         ]);
 
         $valid['image'] = str_replace(' ', '', rand() . now()->toDateTimeString() . '.' . $valid['image']->extension());
         $request->file('image')->move(public_path(config('dir.profile')), $valid['image']);
-        $valid['dob'] = Carbon::createFromFormat('d-M-Y', $valid['dob']);
+        
+        $valid['dob'] = Carbon::createFromFormat('d-M-Y', $request->dob);
         $valid['account_number'] = generateAccountNumber();
-
-        dd($valid);
+        
         $user = User::find(auth('user')->user()->id);
         $user->update($valid);
         
-        dd($user);
         Mail::to($user)->send(new OnBoardMailable($valid['account_number']));
         session()->flash('success','Your bank account has been created successfully');
         return redirect()->route('user.index');
