9f457f1f93bced7ac14d4e9f94a15802ac74202a
Xpressglobe <tobeodiachi@yahoo.com>
2023-08-21 05:30:14 +0100

    fixec 419 exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,9 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +38,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof TokenMismatchException) {
+            session()->flash('error', 'Page expired. Please reload page and try again.');
+            return back();
+        }
+
+        return parent::render($request, $exception);
     }
 }
